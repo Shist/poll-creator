ae780ddbfb2ef2c7b3fa7c6cb2293b55f3ae88d8
Shist <shist.pupust@mail.ru>
2024-05-24 14:12:38 +0300

    Added getter that returns an array of arrays of ids of free choices

--- src/store/pollDataModule.ts
+++ src/store/pollDataModule.ts
@@ -18,35 +18,47 @@ const pollDataModule = defineModule({
     pollRows: [],
   }),
   getters: {
-    areAllChoicesPresent(state): boolean {
+    getFreeQuestionsChoices(state): { [key: number]: number[] } {
+      const freeChoices: { [key: number]: number[] } = {};
+
       for (const serverQuestion of state.serverQuestions) {
-        const serverChoicesSet = new Set(
-          serverQuestion.choices.map((choice) => choice.value)
+        freeChoices[serverQuestion.id] = [];
+      }
+
+      for (const userQuestion of state.userQuestions) {
+        const userChoicesSet = new Set(
+          userQuestion.choices.map((choice) => choice.id)
         );
 
-        const userQuestion = state.userQuestions.find(
-          (uq) => uq.id === serverQuestion.id
+        const serverQuestion = state.serverQuestions.find(
+          (uq) => uq.id === userQuestion.id
         );
 
-        if (
-          userQuestion &&
-          userQuestion.choices.every((userChoice) =>
-            serverChoicesSet.has(userChoice.value)
-          )
-        ) {
+        if (serverQuestion) {
+          serverQuestion.choices.forEach((serverChoice) => {
+            if (!userChoicesSet.has(serverChoice.id)) {
+              freeChoices[serverChoice.id].push(serverChoice.id);
+            }
+          });
           continue;
-        } else {
-          return false;
         }
       }
 
-      return true;
+      return freeChoices;
+    },
+    areAllChoicesPresent(state, getters): boolean {
+      return Object.values(getters.getFreeQuestionsChoices).every(
+        (choicesIdsArr: number[]) => !choicesIdsArr.length
+      );
     },
     getPollRowsStructure: (state) => (type: "init" | "current") => {
       const result: IPollRowStructure[] = [];
 
       if (type === "init") {
         const qArr = state.serverQuestions;
+
+        const nextQuestionsList = qArr.map((q) => q.name);
+        nextQuestionsList.push("Завершено");
 
         qArr.forEach((q, qIndex) => {
           const mapNextQuestionsChoices: IPollRowInfo = {};
@@ -72,20 +84,15 @@ const pollDataModule = defineModule({
               const nextPollRow: IPollRowStructure = {
                 rowId: rowInfoObj.rowId,
                 selectValFirst: rowInfoObj.qName,
-                selectOptionsFirst: [rowInfoObj.qName],
+                selectOptionsFirst: [],
                 selectValsSecond: rowInfoObj.choicesArr,
-                selectOptionsSecond: q.choices.map((choice) => choice.value),
+                selectOptionsSecond: [],
                 selectValThird:
                   nextQuestionIdStr === "null"
                     ? "Завершено"
                     : qArr.find((q) => q.id === Number(nextQuestionIdStr))
                         ?.name,
-                selectOptionsThird: [
-                  nextQuestionIdStr === "null"
-                    ? "Завершено"
-                    : qArr.find((q) => q.id === Number(nextQuestionIdStr))
-                        ?.name,
-                ],
+                selectOptionsThird: nextQuestionsList,
               };
 
               result.push(nextPollRow);
